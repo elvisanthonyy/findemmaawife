d3ec66981a5dfaa73a639f89d25be3cb0f4697b4
elvisanthonyy <info.elvisanthony@gmail.com>
2026-02-11 12:50:04 +0100

    final

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const infoBtn = document.querySelector(".info-btn");
 const info = document.querySelector(".info-cont");
 const mainInfo = document.querySelector(".main-info");
+const angle = document.querySelector(".angle");
 
 infoBtn.addEventListener("click", () => {
+  angle.classList.toggle("angle-active");
   //info.scrollIntoView();
   if (info.classList.contains("info-cont-active")) {
     info.classList.remove("info-cont-active");
